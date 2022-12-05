e25f164791c7166c668a9b8db7c5dcb308e66537
Carl Lundin <carllundin55@gmail.com>
2022-12-04 22:03:49 -0800

    Finished day three part two.

--- three/src/lib.rs
+++ three/src/lib.rs
@@ -29,9 +29,38 @@ pub fn sum_of_duplicate_items(packing_list: &str) -> u32 {
         let intersection = first_rucksack.intersection(&second_rucksack);
         intersection.into_iter().nth(0).cloned()
     });
-    let sum_of_duplicate_items = duplicate_items.map(|item| char_to_point(&item)).sum();
 
-    sum_of_duplicate_items
+    duplicate_items.map(|item| char_to_point(&item)).sum()
+}
+
+pub fn sum_of_badge_items(packing_list: &str, group_size: usize) -> u32 {
+    let mut duplicate_items = Vec::new();
+    let mut packing_list_items = packing_list.lines().peekable();
+
+    while packing_list_items.peek().is_some() {
+        let packed_items: Vec<HashSet<char>> = packing_list_items
+            .by_ref()
+            .take(group_size)
+            .map(|line| {
+                let h: HashSet<char> = line.chars().collect();
+                h
+            })
+            .collect();
+
+        let last_rucksack = packed_items.last().unwrap();
+        let badge = last_rucksack.iter().find_map(|c| {
+            if packed_items.iter().all(|s| s.contains(&c)) {
+                return Some(c);
+            }
+            None
+        });
+
+        if let Some(badge) = badge {
+            duplicate_items.push(char_to_point(badge));
+        }
+    }
+
+    duplicate_items.into_iter().sum()
 }
 
 #[cfg(test)]
@@ -46,5 +75,15 @@ mod tests {
     #[test]
     fn test_puzzle() {
         assert_eq!(sum_of_duplicate_items(PUZZLE_INPUT), 7727);
+    }
+
+    #[test]
+    fn test_sample_part_two() {
+        assert_eq!(sum_of_badge_items(EXAMPLE, 3), 70);
+    }
+
+    #[test]
+    fn test_puzzle_part_two() {
+        assert_eq!(sum_of_badge_items(PUZZLE_INPUT, 3), 2609);
     }
 }
