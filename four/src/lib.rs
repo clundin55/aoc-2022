468444d7bdb97006a2dac1e975e2f541b9bcbd02
Carl Lundin <carllundin55@gmail.com>
2022-12-05 18:09:07 -0800

    Finished day four part two.

--- four/src/lib.rs
+++ four/src/lib.rs
@@ -19,6 +19,10 @@ impl ElfAssignment {
 
         false
     }
+
+    fn is_overlapping(&self, other: &Self) -> bool {
+        self.lower <= other.upper && other.lower <= self.upper
+    }
 }
 
 impl From<&str> for ElfAssignment {
@@ -40,20 +44,42 @@ impl From<&str> for ElfAssignment {
     }
 }
 
-pub fn overlapping_assignments(input: &str) -> u32 {
-    let assignments = input.lines().filter_map(|line| {
+fn create_assignment_list(input: &str) -> Vec<(ElfAssignment, ElfAssignment)> {
+    let assignments = input.lines().map(|line| {
         let line = line.split_once(",").expect("Could not split assignments.");
         let elf1 = ElfAssignment::from(line.0);
         let elf2 = ElfAssignment::from(line.1);
 
-        if elf1.is_subset_or_superset(&elf2) {
-            return Some(elf1);
-        }
-
-        None
+        (elf1, elf2)
     });
 
-    assignments.count() as u32
+    assignments.collect()
+}
+
+pub fn redudant_assignments(input: &str) -> u32 {
+    let assignments = create_assignment_list(input);
+
+    let redudant_pairs = assignments.iter().filter(|pair| {
+        let elf1 = &pair.0;
+        let elf2 = &pair.1;
+
+        elf1.is_subset_or_superset(&elf2)
+    });
+
+    redudant_pairs.count() as u32
+}
+
+pub fn overlapping_assignments(input: &str) -> u32 {
+    let assignments = create_assignment_list(input);
+
+    let redudant_pairs = assignments.iter().filter(|pair| {
+        let elf1 = &pair.0;
+        let elf2 = &pair.1;
+
+        elf1.is_overlapping(&elf2)
+    });
+
+    redudant_pairs.count() as u32
 }
 
 #[cfg(test)]
@@ -62,11 +88,21 @@ mod tests {
 
     #[test]
     fn test_sample() {
-        assert_eq!(overlapping_assignments(EXAMPLE), 2);
+        assert_eq!(redudant_assignments(EXAMPLE), 2);
     }
 
     #[test]
     fn test_puzzle() {
+        assert_eq!(redudant_assignments(PUZZLE_INPUT), 536);
+    }
+
+    #[test]
+    fn test_sample_part_two() {
+        assert_eq!(overlapping_assignments(EXAMPLE), 4);
+    }
+
+    #[test]
+    fn test_puzzle_part_two() {
         assert_eq!(overlapping_assignments(PUZZLE_INPUT), 2);
     }
 }
